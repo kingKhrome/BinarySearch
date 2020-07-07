7e369be192320fb7f6d0a816f0e1c46d15eb8d18
Glitch (oval-jeweled-raptorex) <none>
2020-07-07 20:46:43 +0000

    🐮🐪 Checkpoint ./script.js:18588164/2580

--- script.js
+++ script.js
@@ -5,37 +5,100 @@
           mouseX, mouseY, strokeWeight, line, mouseIsPressed, noFill, windowWidth, windowHeight, noStroke, 
           keyCode, PI, HALF_PI, UP_ARROW, LEFT_ARROW, RIGHT_ARROW, DOWN_ARROW, textSize */
 
+let target = Number(prompt('Enter an integer from 1-25, or not: '))
+
+function wait(s) {
+  let time = Date.now();
+  let t = null;
+  while (t - time < s) {
+    t = Date.now(); }
+}
+// Copied sleep function
+let array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+let upper = array.length-1
+let lower = 0
+
+
 function setup() {
-  createCanvas(400, 400);
+	createCanvas(1000, 600);
+	strokeWeight(0)
+}
+
+
+function show() {
+  textSize(30);
+  textAlign(CENTER);
+  text('Target: ' +target,500, 50 )
+  textSize(12);
+  textAlign(LEFT);
+  for(let i = 0; i<array.length; i++) {
+    fill('gray');
+    if (i === upper) {
+      fill('blue');
+      rect(35*i+50,500,30, -10*array[i]);
+      text('Upper',35*i+50,540);
+      text(array[i],35*i+60,520);
+    }
+    if (i == lower) {
+      fill('red') ;
+      rect(35*i+50,500,30, -10*array[i]);
+      text('Lower',35*i+50,540);
+      text(array[i],35*i+60,520);
+    }
+    else {
+    rect(35*i+50,500,30, -10*array[i]);
+    text(array[i],35*i+60,520);
+    fill('gray');
+  } 
+  }
+  }
+
+
+function valFound(index) {
+    wait(1000);
+    fill('gold') ;
+    rect(35*index+50,500,30, -10*array[index]);
+    text(array[index],35*index+60,520);
+    textSize(30)
+    textAlign(CENTER)
+    text('Found! Index = ' +index,500, 100 )
+    noLoop()
+  
+}
+function notFound(index) {
+    wait(1000);
+    fill('purple') ;
+    rect(35*index+50,500,30, -10*array[index]);
+    text(array[index],35*index+60,520);
+    textSize(30)
+    textAlign(CENTER)
+    text('Not Found' ,500, 100 )
+    noLoop()
+  
+}
+function binarySearch() {
+    if (array[upper] === target) return valFound(upper)
+    if (array[lower] === target) return valFound(lower)
+	let change = Math.round((upper+lower)/2);
+    console.log(upper,lower,change)
+	if (array[change] < target) {
+		lower = change;
+	} else {
+         upper = change; 
+       }
+  if (upper === lower) {
+    notFound(change)
+  }
+  if (array[upper] === target || array[lower] === target) {
+  valFound(change); 
+  }
+}
+
+
+
+function draw() {
   background(255);
-  noFill();
-  strokeWeight(5);
-  
-  stroke('black')
-  ellipse(110,150,50);
-  
-  stroke('green')
-  ellipse(140,175,50);
-  
-  stroke('yellow')
-  ellipse(80,175,50);
- 
-  stroke('blue')
-  ellipse(50,150,50);
-  
-  stroke('yellow')
-  arc(80, 175, 50, 50,PI-.3, PI+.3); 
-  
-  stroke('black')
-  arc(110, 150, 50, 50,-PI/2, -PI/2 -.3); 
-  
-  stroke('green')
-  arc(140, 175, 50, 50,PI-.3, PI+.3); 
-
-  stroke('red')
-  ellipse(170,150,50);
-  
-  stroke('green')
-  arc(140, 175, 50, 50,0, -PI/2); 
-
+  show()
+  binarySearch()
+  wait(1000)
 }
